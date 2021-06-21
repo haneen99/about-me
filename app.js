83885ba4dd4dd769c94eabbed615ccc331b36cde
haneen99 <haneenalhaddad99@gmail.com>
2021-06-21 22:41:05 +0300

    js

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+let username = prompt("what's your name?");
+alert("welcome " + username);
+
+let age=prompt("is my age 23").toLowerCase();
+if (age=="yes"){alert("un correct")
+//console.log("un correct")
+}
+else if (age=="no"){alert("that's correct");
+//console.log("that's correct")
+}
+let food=prompt("is my favourite food is pizza" ).toLowerCase();
+if (food=="yes"){alert("correct")
+//console.log("correct")
+}
+else if (food=="no"){alert("that's un correct");
+//console.log("that's un correct")
+}
+let color=prompt("is my favourite color black" ).toLowerCase();
+if (color=="no"){alert("correct")
+//console.log("correct")
+}
+else if (color=="yes"){alert("that's un correct");
+//console.log("that's un correct")
+}
+let nationality=prompt("is my nationality palestanine" ).toLowerCase();
+if (nationality=="yes"){alert("correct")
+//console.log("correct")
+}
+else if (nationality=="no"){alert("that's un correct");
+//console.log("that's un correct")
+}
+
+let nature=prompt("do i like nature" ).toLowerCase();
+if (nature=="yes"){alert("correct")
+//console.log("correct")
+}
+else if (nature=="no"){alert("that's un correct");
+//console.log("that's un correct")
+}
+
