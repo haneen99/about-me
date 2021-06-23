e0238c517d7ad44bfa0f14dcf77f9fe349dbb9b0
haneen99 <haneenalhaddad99@gmail.com>
2021-06-23 14:38:28 +0300

    addjs

--- app.js
+++ app.js
@@ -39,3 +39,49 @@ else if (nature=="no"){alert("that's un correct");
 //console.log("that's un correct")
 }
 
+let i;
+let answer =[];
+for(i= 1; i<= 4;i++){
+   let question = Number (prompt("guess my age"));
+   answer.push(question);
+   if(answer===22){
+       alert('you are right');
+       dgree++;
+       break;
+} else if (answer>22){
+    alert ("sorry un correct")
+
+}else if (answer <22){
+    alert("no im upset")
+}
+alert("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
+console.log("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
+alert ("im 22");
+console.log("im 22");
+let myplaces =["japan","china","india","iraq","syria","egybt","eran"];
+console.log(myplaces);
+let q;
+for(let n=1;n<=6; n++){
+    q = prompt("guess my favourite places?attempt number"+n).toLowerCase();
+    console.log(q);
+    for(let i=0; i<myplaces.length; i++){
+        if(q == myplaces[i]){
+        alert("im so lucky of you");
+        dgree++;
+        break;
+        }
+    }
+    /*if (myplaces.includes(q)){
+        alert("im so lucky of you")
+        dgree++;
+        break;
+
+    }else{
+        alert("myplaces:"+myplaces);
+        console.log("myplaces:"+myplaces);
+        alert("you are excellent.yourscore:"+grade );
+        console.log(grade);
+    }*/
+
+
+}
