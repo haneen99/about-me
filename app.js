49194e21a3a98b64a0dd5c801698516aeecd959a
Ahmad Yousef <dawabsheh@engineer.com>
2021-06-23 19:58:37 +0300

    edited and added functions by ahmad yousef

--- app.js
+++ app.js
@@ -2,74 +2,114 @@
 let username = prompt("what's your name?");
 alert("welcome " + username);
 
-let age=prompt("is my age 23").toLowerCase();
-if (age=="yes"){alert("un correct")
-//console.log("un correct")
-}
-else if (age=="no"){alert("that's correct");
-//console.log("that's correct")
-}
-let food=prompt("is my favourite food is pizza" ).toLowerCase();
-if (food=="yes"){alert("correct")
-//console.log("correct")
-}
-else if (food=="no"){alert("that's un correct");
-//console.log("that's un correct")
-}
-let color=prompt("is my favourite color black" ).toLowerCase();
-if (color=="no"){alert("correct")
-//console.log("correct")
-}
-else if (color=="yes"){alert("that's un correct");
-//console.log("that's un correct")
-}
-let nationality=prompt("is my nationality palestanine" ).toLowerCase();
-if (nationality=="yes"){alert("correct")
-//console.log("correct")
-}
-else if (nationality=="no"){alert("that's un correct");
-//console.log("that's un correct")
+
+
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
+question7();
+
+
+
+
+
+function question1(){
+    let age=prompt("is my age 23").toLowerCase();
+    if (age=="yes"){
+        alert("un correct")
+    }else if (age=="no")
+    {alert("that's correct");
+    //console.log("that's correct")
+    }
+    //console.log("un correct")
 }
 
-let nature=prompt("do i like nature" ).toLowerCase();
-if (nature=="yes"){alert("correct")
-//console.log("correct")
-}
-else if (nature=="no"){alert("that's un correct");
-//console.log("that's un correct")
+function question2(){
+    let food=prompt("is my favourite food is pizza" ).toLowerCase();
+    if (food=="yes"){alert("correct")
+    //console.log("correct")
+    }
+    else if (food=="no"){alert("that's un correct");
+    //console.log("that's un correct")
+    }
 }
 
-let i;
-let answer =[];
-for(i= 1; i<= 4;i++){
-   let question = Number (prompt("guess my age"));
-   answer.push(question);
-   if(answer===22){
-       alert('you are right');
-       dgree++;
-       break;
-} else if (answer>22){
-    alert ("sorry un correct")
-
-}else if (answer <22){
-    alert("no im upset")
+function question3(){
+    let color=prompt("is my favourite color black" ).toLowerCase();
+    if (color=="no"){alert("correct")
+    //console.log("correct")
+    }
+    else if (color=="yes"){alert("that's un correct");
+    //console.log("that's un correct")
+    }
 }
-alert("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
-console.log("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
-alert ("im 22");
-console.log("im 22");
-let myplaces =["japan","china","india","iraq","syria","egybt","eran"];
+
+function question4(){
+    let nationality=prompt("is my nationality palestanine" ).toLowerCase();
+    if (nationality=="yes"){alert("correct")
+    //console.log("correct")
+    }
+    else if (nationality=="no"){alert("that's un correct");
+    //console.log("that's un correct")
+    }
+}
+
+function question5(){
+    let nature=prompt("do i like nature" ).toLowerCase();
+    if (nature=="yes"){alert("correct")
+    //console.log("correct")
+    }
+    else if (nature=="no"){alert("that's un correct");
+    //console.log("that's un correct")
+    }
+}
+
+function question6(){
+    let i;
+    let dgree = 0;
+    let answer =[];
+    for(i= 1; i<= 4;i++){
+    let question = Number (prompt("guess my age"));
+    answer.push(question);
+    if(answer==22){
+        alert('you are right');
+        dgree++;
+        break;
+        } else if (answer>22){
+            alert ("sorry un correct")
+
+        }else if (answer <22){
+            alert("no im upset")
+        }
+    }
+    alert("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
+    console.log("your answer:"+answer[0]+"/"+answer[1]+"/"+answer[2]+"/"+answer[3]);
+    alert ("im 22");
+    console.log("im 22");
+}
+
+function question7(){
+    let myplaces =["japan","china","india","iraq","syria","egybt","eran"];
 console.log(myplaces);
 let q;
+let corect = 0;
 for(let n=1;n<=6; n++){
     q = prompt("guess my favourite places?attempt number"+n).toLowerCase();
     console.log(q);
-    for(let i=0; i<myplaces.length; i++){
-        if(q == myplaces[i]){
+    for(let h=0; h<myplaces.length; h++){
+        if(q == myplaces[h]){
         alert("im so lucky of you");
         dgree++;
+        corect = 1;
+        console.log('Correct = ' + corect);
         break;
         }
+    }
+    if(corect == 1){
+        break;
     }
     /*if (myplaces.includes(q)){
         alert("im so lucky of you")
@@ -84,4 +124,5 @@ for(let n=1;n<=6; n++){
     }*/
 
 
+}
 }
